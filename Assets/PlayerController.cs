d0a2b83d67f2ad29210edd0e80c6a834e2d1b8c1
Peter Gerasimov <gerasimov@mediahub.eu>
2025-12-13 02:09:19 +0200

    treewide: probuilde + more player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField]
 	private float _speed = 5f;
+	[SerializeField]
+	private float _maxSpeed = 7f;
+	[SerializeField]
+	private float _jumpForce = 5f;
 
 	[SerializeField]
 	private Transform _cameraTransform;
@@ -24,11 +28,23 @@ public class PlayerController : MonoBehaviour
 			_cameraTransform.forward * _moveInput.y + _cameraTransform.right * _moveInput.x;
 		move.y = 0f;
 		_rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
+
+		Vector3 horizontalVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
+		if (horizontalVelocity.magnitude > _maxSpeed) {
+			horizontalVelocity = horizontalVelocity.normalized * _maxSpeed;
+			_rb.linearVelocity = new Vector3(horizontalVelocity.x, _rb.linearVelocity.y, horizontalVelocity.z);
+		}
 	}
 
 	public void OnMove(InputValue value)
 	{
 		_moveInput = value.Get<Vector2>();
-		//_moveInput = context.ReadValue<Vector2>();
+	}
+
+	public void OnJump(InputValue value)
+	{
+		if (value.isPressed && Mathf.Abs(_rb.linearVelocity.y) < 0.01f) {
+			_rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+		}
 	}
 }
