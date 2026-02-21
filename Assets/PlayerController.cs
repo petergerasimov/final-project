09b3da9333d5152d03decf3fcff10adf1df61dc0
Peter Gerasimov <gerasimov@mediahub.eu>
2026-02-21 05:51:04 +0200

    player: controller for default physics - not working
    Signed-off-by: Peter Gerasimov <gerasimov@mediahub.eu>

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,52 +1,52 @@
-// TODO: Backport
+using UnityEngine;
+using UnityEngine.InputSystem;
 
-// using UnityEngine;
-// using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private float _speed = 5f;
+	[SerializeField]
+	private float _maxSpeed = 7f;
+	[SerializeField]
+	private float _jumpForce = 5f;
 
-// [RequireComponent(typeof(Rigidbody))]
-// public class PlayerController : MonoBehaviour
-// {
-// 	[SerializeField]
-// 	private float _speed = 5f;
-// 	[SerializeField]
-// 	private float _maxSpeed = 7f;
-// 	[SerializeField]
-// 	private float _jumpForce = 5f;
+	[SerializeField]
+	private Transform _cameraTransform;
 
-// 	[SerializeField]
-// 	private Transform _cameraTransform;
+	private Rigidbody _rb;
+	private Vector2 _moveInput;
 
-// 	private Rigidbody _rb;
-// 	private Vector2 _moveInput;
+	void Start()
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
 
-// 	void Start()
-// 	{
-// 		_rb = GetComponent<Rigidbody>();
-// 	}
+	private void FixedUpdate()
+	{
+		Vector3 move =
+			_cameraTransform.forward * _moveInput.y + _cameraTransform.right * _moveInput.x;
+		move.y = 0f;
+		// _rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
+		// test forward force hardcoded
+		_rb.AddForce(Vector3.forward * _speed, ForceMode.VelocityChange);
 
-// 	private void FixedUpdate()
-// 	{
-// 		Vector3 move =
-// 			_cameraTransform.forward * _moveInput.y + _cameraTransform.right * _moveInput.x;
-// 		move.y = 0f;
-// 		_rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
+		Vector3 horizontalVelocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+		if (horizontalVelocity.magnitude > _maxSpeed) {
+			horizontalVelocity = horizontalVelocity.normalized * _maxSpeed;
+			_rb.velocity = new Vector3(horizontalVelocity.x, _rb.velocity.y, horizontalVelocity.z);
+		}
+	}
 
-// 		Vector3 horizontalVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
-// 		if (horizontalVelocity.magnitude > _maxSpeed) {
-// 			horizontalVelocity = horizontalVelocity.normalized * _maxSpeed;
-// 			_rb.linearVelocity = new Vector3(horizontalVelocity.x, _rb.linearVelocity.y, horizontalVelocity.z);
-// 		}
-// 	}
+	public void OnMove(InputValue value)
+	{
+		_moveInput = value.Get<Vector2>();
+	}
 
-// 	public void OnMove(InputValue value)
-// 	{
-// 		_moveInput = value.Get<Vector2>();
-// 	}
-
-// 	public void OnJump(InputValue value)
-// 	{
-// 		if (value.isPressed && Mathf.Abs(_rb.linearVelocity.y) < 0.01f) {
-// 			_rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
-// 		}
-// 	}
-// }
+	public void OnJump(InputValue value)
+	{
+		if (value.isPressed && Mathf.Abs(_rb.velocity.y) < 0.01f) {
+			_rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+		}
+	}
+}
