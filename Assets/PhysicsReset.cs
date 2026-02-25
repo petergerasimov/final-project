f5c20c2bbbdd0450fd25f3f93673b2141113d78f
Peter Gerasimov <gerasimov@mediahub.eu>
2026-02-25 23:59:15 +0200

    movement: reset original physics and added backported camera movement
    Signed-off-by: Peter Gerasimov <gerasimov@mediahub.eu>

---
+++ Assets/PhysicsReset.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace PhysX5ForUnity
+{
+
+    [DefaultExecutionOrder(0)]
+    public class PhysicsReset : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Physx.GetPhysXInitStatus();
+            Physics.simulationMode = SimulationMode.FixedUpdate;
+        }
+    }
+
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,16 +10,36 @@ public class PlayerController : MonoBehaviour
 	private float _maxSpeed = 7f;
 	[SerializeField]
 	private float _jumpForce = 5f;
+	[SerializeField]
+	private float _lookSensitivity = 0.5f;
 
 	[SerializeField]
 	private Transform _cameraTransform;
 
 	private Rigidbody _rb;
 	private Vector2 _moveInput;
+	private Vector2 _lookInput;
+	private float _xRotation = 0f;
 
 	void Start()
 	{
 		_rb = GetComponent<Rigidbody>();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	private void Update()
+	{
+		float mouseX = _lookInput.x * _lookSensitivity;
+		float mouseY = _lookInput.y * _lookSensitivity;
+
+		_xRotation -= mouseY;
+		_xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+		if (_cameraTransform != null) {
+			_cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+		}
+		transform.Rotate(Vector3.up * mouseX);
 	}
 
 	private void FixedUpdate()
@@ -27,9 +47,7 @@ public class PlayerController : MonoBehaviour
 		Vector3 move =
 			_cameraTransform.forward * _moveInput.y + _cameraTransform.right * _moveInput.x;
 		move.y = 0f;
-		// _rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
-		// test forward force hardcoded
-		_rb.AddForce(Vector3.forward * _speed, ForceMode.VelocityChange);
+		_rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
 
 		Vector3 horizontalVelocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
 		if (horizontalVelocity.magnitude > _maxSpeed) {
@@ -41,6 +59,11 @@ public class PlayerController : MonoBehaviour
 	public void OnMove(InputValue value)
 	{
 		_moveInput = value.Get<Vector2>();
+	}
+
+	public void OnLook(InputValue value)
+	{
+		_lookInput = value.Get<Vector2>();
 	}
 
 	public void OnJump(InputValue value)
